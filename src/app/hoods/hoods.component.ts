c9b60d671135df768af7a5dff4573aae312e6b3c
Rustique-Uwimpaye <rustique644@gmail.com>
2020-12-03 23:06:22 +0300

    created a hoods component

---
+++ src/app/hoods/hoods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hoods',
+  templateUrl: './hoods.component.html',
+  styleUrls: ['./hoods.component.css']
+})
+export class HoodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
