d898e87f017db6ba3ad1bfc603931b44b2fd1b8d
Rustique-Uwimpaye <rustique644@gmail.com>
2020-12-04 08:08:27 +0300

    src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HoodsComponent } from './hoods/hoods.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  }, {
+    path: "profile",
+    component: ProfileComponent 
+  }, {
+    path: "hoods",
+    component: HoodsComponent
+  }
+];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
