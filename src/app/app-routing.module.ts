de8a10c7e11d9ca1846d45a5cf98cba585348db2
Paulyne <paulynewambui2020@gmail.com>
2020-12-05 20:14:42 +0300

    add the login and register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HoodsComponent } from './hoods/hoods.component';
+import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -14,6 +16,12 @@ const routes: Routes = [
   }, {
     path: "hoods",
     component: HoodsComponent
+  }, {
+    path: "login",
+    component: LoginComponent
+  },{
+    path: "register",
+    component: RegisterComponent
   }
 ];
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// MDB Angular Free
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarModule } from 'ng-sidebar';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { HoodsComponent } from './hoods/hoods.component'
+import { HoodsComponent } from './hoods/hoods.component';
+import { LoginComponent } from './login/login.component';
+import bootstrap from "bootstrap";
+import { RegisterComponent } from './register/register.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProfileComponent,
-    HoodsComponent
+    HoodsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
